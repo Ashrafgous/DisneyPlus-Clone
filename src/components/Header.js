63844e35d1463170f5578b0ab68c12aa2bff0ec7
mohammadgous1719 <mohammadgous1719@gmail.com>
2021-06-11 10:33:56 +0530

    2. Header Added

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,38 @@ import styled from "styled-components";
 function Header() {
   return (
     <Nav>
-      <Logo src="" />
-      <NavMenu></NavMenu>
+      <Logo src="/images/logo.svg" />
+      <NavMenu>
+        <a href="">
+          <img src="/images/home-icon.svg" alt="" />
+          <span>HOME</span>
+        </a>
+        <a href="">
+          <img src="/images/search-icon.svg" alt="" />
+          <span>SEARCH</span>
+        </a>
+        <a href="">
+          <img src="/images/watchlist-icon.svg" alt="" />
+          <span>WATCHLIST</span>
+        </a>
+        <a href="">
+          <img src="/images/search-icon.svg" alt="" />
+          <span>SEARCH</span>
+        </a>
+        <a href="">
+          <img src="/images/original-icon.svg" alt="" />
+          <span>ORIGINALS</span>
+        </a>
+        <a href="">
+          <img src="/images/movie-icon.svg" alt="" />
+          <span>MOVIES</span>
+        </a>
+        <a href="">
+          <img src="/images/series-icon.svg" alt="" />
+          <span>SERIES</span>
+        </a>
+      </NavMenu>
+      <UserImg src="http://kurtliuphotography.com/wp-content/uploads/2016/05/D4S_2679.jpg" />
     </Nav>
   );
 }
@@ -15,8 +45,58 @@ export default Header;
 const Nav = styled.nav`
   height: 78px;
   background: #090b13;
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
 `;
 const Logo = styled.img`
   width: 80px;
 `;
-const NavMenu = styled.div``;
+const NavMenu = styled.div`
+  display: flex;
+  flex: 1;
+  margin-left: 25px;
+  align-items: center;
+
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    text-decoration: none;
+    cursor: pointer;
+    img {
+      height: 20px;
+    }
+    span {
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      position: relative;
+
+      &:after {
+        content: "";
+        height: 2px;
+        background: white;
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: -6px;
+        opacity: 0;
+        transform-origin: left center;
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+        transform: scaleX(0);
+      }
+    }
+    &:hover {
+      span:after {
+        transform: scaleX(1);
+        opacity: 1;
+      }
+    }
+  }
+`;
+const UserImg = styled.img`
+  height: 48px;
+  width: 48px;
+  border-radius: 50%;
+  cursor: pointer;
+`;
